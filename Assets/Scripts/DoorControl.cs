bef315187a5401acad97e4d4a693213e78348d51
Another Cat <Another Cat>
2017-04-11 10:44:20 +0700

    active button and door

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour {
+
+    public static DoorControl Instance;
+
+    public Animator[] doorAnimators;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDoor(int index)
+    {
+        if(!doorAnimators[index].GetCurrentAnimatorStateInfo(0).IsName("open") || !doorAnimators[index].GetCurrentAnimatorStateInfo(0).IsName("close"))
+        {
+            doorAnimators[index].SetTrigger("open");
+        }
+    }
+
+    public void CloseDoor(int index)
+    {
+        if (!doorAnimators[index].GetCurrentAnimatorStateInfo(0).IsName("open") || !doorAnimators[index].GetCurrentAnimatorStateInfo(0).IsName("close"))
+        {
+            doorAnimators[index].SetTrigger("close");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRaycast : MonoBehaviour {
+
+    public Camera FPSCam;
+    public GameObject HandPanel;
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+		if(Physics.Raycast(FPSCam.transform.position,FPSCam.transform.forward,out hit, 2))
+        {
+            if (hit.collider.name.StartsWith("button1"))
+            {
+                HandPanel.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DoorControl.Instance.OpenDoor(0);
+                }
+            }
+            else
+            {
+                hideAllUI();
+            }
+        }
+        else
+        {
+            hideAllUI();
+        }
+	}
+
+    void hideAllUI()
+    {
+        HandPanel.SetActive(false);
+    }
+}
